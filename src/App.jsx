f18bbb5e21a10736073a4b15b99d7733c3960ceb
Hikmatjan <125250004+Hikmatjan@users.noreply.github.com>
2024-05-04 23:44:42 +0500

    first-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,81 @@
 import React from "react";
-import { Wrapper } from "./style";
+import { useState, useEffect } from "react";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+  DownOutlined,
+} from "@ant-design/icons";
+import { Avatar, Card, Dropdown, Space } from "antd";
+const items = [
+  {
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.antgroup.com"
+      >
+        1st menu item
+      </a>
+    ),
+    key: "0",
+  },
+  {
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.aliyun.com"
+      >
+        2nd menu item
+      </a>
+    ),
+    key: "1",
+  },
+  {
+    type: "divider",
+  },
+];
+
+const { Meta } = Card;
+
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
-    <div className="bg-[#46A358]">
-      <Wrapper>
-        <Wrapper.Left>
-          <img
-            className="size"
-            src="https://babm.uz/thogeeck/2022/07/logo-babm.png"
-            alt="Logo"
-          />
-        </Wrapper.Left>
-        <Wrapper.Middle>
-          <Wrapper.Middle.Item>Yuriqnoma</Wrapper.Middle.Item>
-          <Wrapper.Middle.Item>Videodarslar</Wrapper.Middle.Item>
-        </Wrapper.Middle>
-        <Wrapper.Right></Wrapper.Right>
-      </Wrapper>
+    <div>
+      {data.map((value) => {
+        return (
+          <Card
+            key={value.id}
+            style={{ width: 300 }}
+            cover={
+              <img
+                alt="example"
+                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+              />
+            }
+            actions={[
+              <SettingOutlined key="setting" />,
+              <EditOutlined key="edit" onClick={(e) => e.preventDefault()} />,
+              <EllipsisOutlined key="ellipsis" />,
+            ]}
+          >
+            <Meta
+              avatar={
+                <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />
+              }
+              title={value.title}
+            />
+          </Card>
+        );
+      })}
     </div>
   );
 };
--- src/style.js
+++ src/style.js
@@ -1,30 +1,41 @@
+// import styled from "styled-components";
+
+// const Wrapper = styled.div`
+//   width: 80%;
+//   height: 78px;
+//   margin: auto;
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+//   border-bottom: 1px solid green;
+// `;
+// Wrapper.Left = styled.div`
+//   .size {
+//     width: 20%;
+//     height: 20%;
+//   }
+// `;
+// Wrapper.Middle = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   gap: 10px;
+// `;
+// Wrapper.Middle.Item = styled.div`
+//   font-size: 16px;
+//   margin: auto;
+// `;
+// Wrapper.Right = styled.div``;
+
+// export { Wrapper };
+
 import styled from "styled-components";
-
 const Wrapper = styled.div`
-  width: 80%;
-  height: 78px;
-  margin: auto;
+  /* width: 300px;
+  height: 300px; */
   display: flex;
+  justify-content: center;
   align-items: center;
-  justify-content: space-between;
-  border-bottom: 1px solid green;
+  background-color: aqua;
 `;
-Wrapper.Left = styled.div`
-  .size {
-    width: 20%;
-    height: 20%;
-  }
-`;
-Wrapper.Middle = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 10px;
-`;
-Wrapper.Middle.Item = styled.div`
-  font-size: 16px;
-  margin: auto;
-`;
-Wrapper.Right = styled.div``;
-
 export { Wrapper };
